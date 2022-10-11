a3efb5b6971e888ef1d51fcff8f5585ae3b18560
Etenanl <40615767+Etenanl@users.noreply.github.com>
2022-10-11 22:58:50 +0800

    Update common.go

--- kvraft/common.go
+++ kvraft/common.go
@@ -1,18 +1,30 @@
 package kvraft
 
 const (
-	OK             = "OK"
-	ErrNoKey       = "ErrNoKey"
-	ErrWrongLeader = "ErrWrongLeader"
+	OK             	= "OK"
+	ErrNoKey       	= "ErrNoKey"
+	ErrWrongLeader 	= "ErrWrongLeader"
+	ErrShutdown     = "ErrShutdown"
+	ErrInitElection = "ErrInitElection"
 )
+
+const (
+	GetOperation   		= "Get"
+	PutOperation       	= "Put"
+	AppendOperation 	= "Append"
+
+)
+
 
 type Err string
 
 // Put or Append
 type PutAppendArgs struct {
-	Key   string
-	Value string
-	Op    string // "Put" or "Append"
+	Key			string
+	Value 		string
+	ClientID	int64
+	OPID 		int
+	OP    		string // "Put" or "Append"
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
@@ -22,12 +34,16 @@ type PutAppendReply struct {
 	Err Err
 }
 
-type GetArgs struct {
-	Key string
-	// You'll have to add definitions here.
-}
 
 type GetReply struct {
 	Err   Err
 	Value string
 }
+
+
+type GetArgs struct{
+	Key 		string
+	ClientID	int64
+	OPID 		int
+}
+
